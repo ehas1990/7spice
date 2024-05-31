9b2833490f00310ce8b19cff3e9573ad81e234db
ehas1990 <ehasahamed@gmail.com>
2024-05-31 20:49:25 +0530

    php file added

--- 7/assets/js/home_2_scripts.js
+++ 7/assets/js/home_2_scripts.js
@@ -7,23 +7,40 @@ $(function () {
     //   $(this).height(width);
     // });
 
-
 });
 
-
-// ------------ swiper sliders -----------
 $(document).ready(function () {
 
+    function initSwiper(selector, settings) {
+        return new Swiper(selector, settings);
+    }
+
+    // Common Swiper settings
+    const commonSettings = {
+        spaceBetween: 24,
+        speed: 1000,
+        pagination: {
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        mousewheel: false,
+        keyboard: true,
+        autoplay: {
+            delay: 5000,
+        },
+        loop: true,
+    };
 
     // ------------ tc-header-slider1 -----------
-    var swiper = new Swiper('.tc-header-st2 .header-slider', {
+    initSwiper('.tc-header-st2 .header-slider', {
+        ...commonSettings,
         slidesPerView: 1,
-        spaceBetween: 0,
-        // effect: "fade",
-        // direction: "vertical",
+        speed: 1500,
         centeredSlides: true,
         parallax: true,
-        speed: 1500,
         pagination: {
             el: '.tc-header-st2 .swiper-pagination',
             clickable: true,
@@ -32,8 +49,6 @@ $(document).ready(function () {
             nextEl: '.tc-header-st2 .swiper-button-next',
             prevEl: '.tc-header-st2 .swiper-button-prev',
         },
-        mousewheel: false,
-        keyboard: true,
         autoplay: {
             delay: 6000,
         },
@@ -42,11 +57,9 @@ $(document).ready(function () {
             init: function () {
                 var swiper = this;
                 for (var i = 0; i < swiper.slides.length; i++) {
-                    $(swiper.slides[i])
-                        // .find(".img")
-                        .attr({
-                            "data-swiper-parallax": 0.75 * swiper.width,
-                        });
+                    $(swiper.slides[i]).attr({
+                        "data-swiper-parallax": 0.75 * swiper.width,
+                    });
                 }
             },
             resize: function () {
@@ -54,109 +67,58 @@ $(document).ready(function () {
             },
         },
     });
-    
 
     // ------------ services slider -----------
-    var swiper = new Swiper('.tc-services-st2 .services-slider', {
+    initSwiper('.tc-services-st2 .services-slider', {
+        ...commonSettings,
         slidesPerView: 5,
-        spaceBetween: 24,
-        // centeredSlides: true,
-        speed: 1000,
+        breakpoints: {
+            0: { slidesPerView: 1 },
+            480: { slidesPerView: 2 },
+            787: { slidesPerView: 3 },
+            991: { slidesPerView: 4 },
+            1200: { slidesPerView: 4 }
+        },
         pagination: {
             el: '.tc-services-st2 .swiper-pagination',
-            clickable: true,
         },
         navigation: {
             nextEl: '.tc-services-st2 .swiper-next',
             prevEl: '.tc-services-st2 .swiper-prev',
         },
-        mousewheel: false,
-        keyboard: true,
-        autoplay: {
-            delay: 5000,
-        },
-        loop: true,
-        breakpoints: {
-            0: {
-                slidesPerView: 1,
-            },
-            480: {
-                slidesPerView: 2,
-            },
-            787: {
-                slidesPerView: 3,
-            },
-            991: {
-                slidesPerView: 4,
-            },
-            1200: {
-                slidesPerView: 4,
-            }
-        }
     });
 
-
-    // ------------ tc-header-slider1 -----------
-    var swiper = new Swiper('.tc-testimonials-st2 .testimonials-slider', {
+    // ------------ tc-testimonials-slider -----------
+    initSwiper('.tc-testimonials-st2 .testimonials-slider', {
+        ...commonSettings,
         slidesPerView: 1,
-        spaceBetween: 24,
-        // centeredSlides: true,
-        speed: 1000,
         pagination: {
             el: '.tc-testimonials-st2 .swiper-pagination',
-            clickable: true,
         },
         navigation: {
             nextEl: '.tc-testimonials-st2 .swiper-button-next',
             prevEl: '.tc-testimonials-st2 .swiper-button-prev',
         },
-        mousewheel: false,
-        keyboard: true,
-        autoplay: {
-            delay: 5000,
-        },
-        loop: true,
     });
 
-
     // ------------ blog slider -----------
-    var swiper = new Swiper('.tc-blog-st2 .posts-slider', {
+    initSwiper('.tc-blog-st2 .posts-slider', {
+        ...commonSettings,
         slidesPerView: 5,
-        spaceBetween: 24,
-        // centeredSlides: true,
-        speed: 1000,
+        breakpoints: {
+            0: { slidesPerView: 1 },
+            480: { slidesPerView: 2 },
+            787: { slidesPerView: 3 },
+            991: { slidesPerView: 3 },
+            1200: { slidesPerView: 3 }
+        },
         pagination: {
             el: '.tc-blog-st2 .swiper-pagination',
-            clickable: true,
         },
         navigation: {
             nextEl: '.tc-blog-st2 .swiper-next',
             prevEl: '.tc-blog-st2 .swiper-prev',
         },
-        mousewheel: false,
-        keyboard: true,
-        autoplay: {
-            delay: 5000,
-        },
-        loop: true,
-        breakpoints: {
-            0: {
-                slidesPerView: 1,
-            },
-            480: {
-                slidesPerView: 2,
-            },
-            787: {
-                slidesPerView: 3,
-            },
-            991: {
-                slidesPerView: 3,
-            },
-            1200: {
-                slidesPerView: 3,
-            }
-        }
     });
 
 });
-
--- 7/assets/js/inner_pages.js
+++ 7/assets/js/inner_pages.js
@@ -1,109 +1,33 @@
 $(function () {
-
-    // ------------ team images width same height -----------
-    // var images = $(".tc-team-style1 .team-card .img, .img_sm_h");
-    // images.each(function () {
-    //   var width = $(this).width();
-    //   $(this).height(width);
-    // });
-
-    // projects change background 
+    // Projects change background
     $('.tc-bg-services-st6 .service-card').on('mouseenter click', function () {
         var tab_id = $(this).attr('data-tab');
         $('.tc-bg-services-st6 .service-card').removeClass('current');
         $(this).addClass('current');
 
-        $('.glry-img .tab-img ').removeClass('current');
+        $('.glry-img .tab-img').removeClass('current');
         $("#" + tab_id).addClass('current');
 
         if ($(this).hasClass('current')) {
             return false;
         }
     });
-
-
 });
 
-
-// ------------ swiper sliders -----------
 $(document).ready(function () {
-    
+    function initSwiper(container, config) {
+        return new Swiper(container, config);
+    }
 
-    // ------------ tc-header-slider1 -----------
-    var swiper = new Swiper('.tc-services-st5 .services-slider', {
-        slidesPerView: 4,
-        spaceBetween: 60,
-        // centeredSlides: true,
-        speed: 1500,
-        pagination: {
-            el: '.tc-services-st5 .swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.tc-services-st5 .swiper-next',
-            prevEl: '.tc-services-st5 .swiper-prev',
-        },
-        mousewheel: false,
-        keyboard: true,
-        autoplay: {
-            delay: 5000,
-        },
-        loop: true,
-        breakpoints: {
-            0: {
-                slidesPerView: 1,
-            },
-            480: {
-                slidesPerView: 2,
-            },
-            787: {
-                slidesPerView: 3,
-            },
-            991: {
-                slidesPerView: 4,
-            },
-            1200: {
-                slidesPerView: 4,
-            }
-        }
-    });
-
-    // ------------ tc-header-slider1 -----------
-    var swiper = new Swiper('.tc-testimonials-st5 .testimonials-slider', {
-        slidesPerView: 1,
+    var defaultConfig = {
         spaceBetween: 24,
-        // centeredSlides: true,
         speed: 1000,
         pagination: {
-            el: '.tc-testimonials-st5 .swiper-pagination',
             clickable: true,
         },
         navigation: {
-            nextEl: '.tc-testimonials-st5 .swiper-button-next',
-            prevEl: '.tc-testimonials-st5 .swiper-button-prev',
-        },
-        mousewheel: false,
-        keyboard: true,
-        autoplay: {
-            delay: 5000,
-        },
-        loop: true,
-    });
-
-
-    // ------------ services slider -----------
-    var swiper = new Swiper('.tc-services-st6 .services-slider', {
-        slidesPerView: 5,
-        spaceBetween: 24,
-        // centeredSlides: true,
-        speed: 1000,
-        pagination: {
-            el: '.tc-services-st6 .swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.tc-services-st6 .swiper-next',
-            prevEl: '.tc-services-st6 .swiper-prev',
+            nextEl: '.swiper-next',
+            prevEl: '.swiper-prev',
         },
         mousewheel: false,
         keyboard: true,
@@ -128,55 +52,16 @@ $(document).ready(function () {
                 slidesPerView: 4,
             }
         }
+    };
+
+    var sliders = [
+        { container: '.tc-services-st5 .services-slider', config: $.extend(true, {}, defaultConfig, { slidesPerView: 4, spaceBetween: 60, pagination: { el: '.tc-services-st5 .swiper-pagination' } }) },
+        { container: '.tc-testimonials-st5 .testimonials-slider', config: $.extend(true, {}, defaultConfig, { slidesPerView: 1, spaceBetween: 24, speed: 1000, pagination: { el: '.tc-testimonials-st5 .swiper-pagination' }, navigation: { nextEl: '.tc-testimonials-st5 .swiper-button-next', prevEl: '.tc-testimonials-st5 .swiper-button-prev' } }) },
+        { container: '.tc-services-st6 .services-slider', config: $.extend(true, {}, defaultConfig, { slidesPerView: 5 }) },
+        { container: '.tc-testimonials-st6 .testimonials-slider', config: $.extend(true, {}, defaultConfig, { spaceBetween: 30, speed: 1500, pagination: { el: '.tc-testimonials-st6 .swiper-pagination' }, breakpoints: { 787: { slidesPerView: 2 }, 991: { slidesPerView: 3 }, 1200: { slidesPerView: 3 } } }) }
+    ];
+
+    sliders.forEach(function(slider) {
+        initSwiper(slider.container, slider.config);
     });
-
-
-    // ------------ services slider -----------
-    var swiper = new Swiper('.tc-testimonials-st6 .testimonials-slider', {
-        // slidesPerView: 5,
-        spaceBetween: 30,
-        // centeredSlides: true,
-        speed: 1500,
-        pagination: {
-            el: '.tc-testimonials-st6 .swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.tc-testimonials-st6 .swiper-next',
-            prevEl: '.tc-testimonials-st6 .swiper-prev',
-        },
-        mousewheel: false,
-        keyboard: true,
-        autoplay: {
-            delay: 5000,
-        },
-        loop: true,
-        breakpoints: {
-            0: {
-                slidesPerView: 1,
-            },
-            480: {
-                slidesPerView: 2,
-            },
-            787: {
-                slidesPerView: 2,
-            },
-            991: {
-                slidesPerView: 3,
-            },
-            1200: {
-                slidesPerView: 3,
-            }
-        }
-    });
-
-});
-
-
-
-// ------------ gsap scripts -----------
-$(function () {
-
-    
-
 });
